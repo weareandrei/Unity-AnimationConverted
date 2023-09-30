27587cef49873f0d720c1449d1ab82df61a55c23
weareandrei <weareandrei@gmail.com>
2023-09-30 23:36:57 +0300

    Spritesheet creating logic is working. But math for grid is not setup correctly. + Files not removed after being used.

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const multer = require('multer')
 const sharp = require('sharp')
 const cors = require('cors')
 const fs = require('fs')
-const path = require('path')
+const map = require("lodash/map")
+const StreamToBuffer = require('stream-to-buffer')
 
 const PSD = require('psd')
-const PNG = require('pngjs').PNG;
+const PNG = require('pngjs').PNG
 
-const app = express();
+const app = express()
 
 app.use(cors()) // Cross-Origin Resource Sharing
 
@@ -39,9 +40,29 @@ app.post('/psdToSpritesheet', upload.array('files'), async (req, res) => {
 
         // Open the PSD file
         PSD.open(psdPath)
-            .then((psd) => {
+            .then(async (psd) => {
+                const layers = await getPsdLayers(psd, psdPath)
+                const layerPng = fs.readFileSync(layers[0])
+
+                return sharp({
+                    create: {
+                        width: 3000,
+                        height: 3000,
+                        channels: 4, // 4 channels for RGBA
+                        background: { r: 255, g: 255, b: 255, alpha: 0 } // Transparent background
+                    }
+                })
+                .composite(
+                    map(layers, (layer, index) => ({
+                        input: layer,
+                        top: index * 1000,
+                        left: index * 1000
+                    }))
+                )
+                .toFile(pngPath)
+
                 // Save the PSD as PNG
-                return psd.image.saveAsPng(pngPath)
+                // return psd.image.saveAsPng(pngPath)
             })
             .then(() => {
                 const pngBuffer = fs.readFileSync(pngPath)
@@ -55,13 +76,40 @@ app.post('/psdToSpritesheet', upload.array('files'), async (req, res) => {
             })
             .finally(() => {
                 // Clean up temporary files
-                fs.unlinkSync(psdPath)
-                fs.unlinkSync(pngPath)
-            });
+                // fs.unlinkSync(psdPath)
+                // fs.unlinkSync(pngPath)
+            })
     } catch (error) {
         console.error('Error processing files:', error)
         res.status(500).json({ error: 'An error occurred while processing files.' })
     }
 })
+
+const getPsdLayers = async (psd, fileName) => {
+    const layersPsd = psd.tree().descendants();
+    const layerPromises = [];
+
+    for (const node of layersPsd) {
+        const layerFilePath = fileName.replace('.psd', `-${node.name}.png`);
+        layerPromises.push(saveLayerAsPng(node, layerFilePath));
+    }
+
+    try {
+        return await Promise.all(layerPromises);
+    } catch (err) {
+        console.error(err.stack);
+        return [];
+    }
+};
+
+async function saveLayerAsPng(node, layerFilePath) {
+    try {
+        await node.saveAsPng(layerFilePath);
+        return layerFilePath;
+    } catch (err) {
+        console.error(err.stack);
+        return null; // or some value indicating failure
+    }
+}
 
 app.listen(8081, () => console.log('App started'))
