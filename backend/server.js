3894e34aff54e1fdf505d414fd77b92aa793aff7
weareandrei <weareandrei@gmail.com>
2023-09-30 21:42:44 +0300

    Clean-up

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const PNG = require('pngjs').PNG;
 
 const app = express();
 
-app.use(cors()); // Cross-Origin Resource Sharing
+app.use(cors()) // Cross-Origin Resource Sharing
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const tmpStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const tempDir = './temp'; // Create a 'temp' directory in your project
+        const tempDir = './temp' // Create a 'temp' directory in your project
         if (!fs.existsSync(tempDir)) {
             fs.mkdirSync(tempDir)
         }
@@ -32,37 +32,36 @@ const upload = multer({ storage: tmpStorage })
 
 app.post('/psdToSpritesheet', upload.array('files'), async (req, res) => {
     try {
-        const file = req.files[0];
+        const file = req.files[0]
 
-        const psdPath = file.path;
-        const pngPath = psdPath.replace('.psd', '.png');
+        const psdPath = file.path
+        const pngPath = psdPath.replace('.psd', '.png')
 
         // Open the PSD file
         PSD.open(psdPath)
             .then((psd) => {
                 // Save the PSD as PNG
-                return psd.image.saveAsPng(pngPath);
+                return psd.image.saveAsPng(pngPath)
             })
             .then(() => {
-                // Read the saved PNG file and get its buffer
-                const pngBuffer = fs.readFileSync(pngPath);
-
-                // Set the content type and send the PNG buffer as a response
-                res.contentType('image/png').send(pngBuffer);
+                const pngBuffer = fs.readFileSync(pngPath)
+                res.contentType('image/png')
+                    .send(pngBuffer)
             })
             .catch((err) => {
-                console.error('Error:', err);
-                res.status(500).json({ error: 'An error occurred while processing files.' });
+                console.error('Error:', err)
+                res.status(500)
+                    .json({ error: 'An error occurred while processing files.' })
             })
             .finally(() => {
                 // Clean up temporary files
-                fs.unlinkSync(psdPath);
-                fs.unlinkSync(pngPath);
+                fs.unlinkSync(psdPath)
+                fs.unlinkSync(pngPath)
             });
     } catch (error) {
-        console.error('Error processing files:', error);
-        res.status(500).json({ error: 'An error occurred while processing files.' });
+        console.error('Error processing files:', error)
+        res.status(500).json({ error: 'An error occurred while processing files.' })
     }
-});
+})
 
-app.listen(8081, () => console.log('App started'));
+app.listen(8081, () => console.log('App started'))
